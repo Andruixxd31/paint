280b0cd02efb13bbd72af091dbb087855fd752ab
Andruixxd31 <andruix93@gmail.com>
2020-04-23 22:03:06 -0500

    made small touches to the code

--- Canvas.java
+++ Canvas.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Canvas extends JPanel {
+public class Canvas extends JPanel implements MouseListener, MouseMotionListener {
 
     /**
      *
@@ -37,6 +40,48 @@ public class Canvas extends JPanel {
         this.setBackground(new Color(46,46,46));
         this.removeAll();
         this.repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
     }
 
 
